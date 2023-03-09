42393b5468c37569449ed9a203582512066589db
Ziedgharbi <117041818+Ziedgharbi@users.noreply.github.com>
2023-03-09 16:06:05 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+import tensorflow as ft
+import tensorflow.keras as keras
+import tensorflow.keras.datasets.imdb as imdb
+
+
+
+
+project_dir= "C:/Users/pc/Nextcloud/Python/GITHUB/Sentimental_analysis_on_hot_encoding/"
+#data_dir=project_dir+"data/"
+
+vocab_size=10000
+
+hide_most_frequently=0
+
+epochs=10
+batch_size=512
+
+
+
